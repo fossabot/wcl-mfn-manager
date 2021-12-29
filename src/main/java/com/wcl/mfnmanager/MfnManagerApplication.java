0a71e8b178dae45313aebeba707e4bb34e6fdeab
Wcl - Martin Warnett <coding.warnettconsultingltd@gmil.com>
2021-12-29 22:29:47 +0000

    Added dependencies and entry point for the JavaFX application

--- src/main/java/com/wcl/mfnmanager/MfnManagerApplication.java
+++ src/main/java/com/wcl/mfnmanager/MfnManagerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MfnManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MfnManagerApplication.class, args);
+		SpringApplication.run(MfnManagerJavaFXApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/wcl/mfnmanager/MfnManagerJavaFXApplication.java
@@ -0,0 +1,10 @@
+package com.wcl.mfnmanager;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MfnManagerJavaFXApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+}
