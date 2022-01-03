7caaab2d28c0002087887646b49dc64c31440984
Wcl - Martin Warnett <coding.warnettconsultingltd@gmil.com>
2022-01-03 21:21:41 +0000

    Added initial basic Spring Boot JavaFX application.

--- src/main/java/com/wcl/mfnmanager/MfnManagerJavaFXApplication.java
+++ src/main/java/com/wcl/mfnmanager/MfnManagerJavaFXApplication.java
@@ -1,10 +1,30 @@
 package com.wcl.mfnmanager;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MfnManagerJavaFXApplication extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
+
+    public static void main(String[] args) {
+        Application.launch();
     }
+
+    @Override
+    public void init() {
+        SpringApplication.run(getClass()).getAutowireCapableBeanFactory().autowireBean(this);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        var helloFxPane = new Pane(new Label("Hello JavaFx"));
+        primaryStage.setScene(new Scene(helloFxPane));
+        primaryStage.show();
+    }
+
 }
